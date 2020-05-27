d4ec5a66a11a21f9be465606a0499453c9aff5e0
Jesus Medina <medinabotamail@gmail.com>
2020-05-27 00:52:12 -0400

    50 porciento

--- nuevocalculo.js
+++ nuevocalculo.js
@@ -7,32 +7,69 @@ var valorDeSalida;
 
 function diferencia(entrada, salida)
 {
-    alert("Hola mundo")
+    var final;
+    var diferencia = salida - entrada
+    if (diferencia == 0)
+    {
+        final = valorDeEntrada
+        return final
+    }
+    if (diferencia == 1)
+    {
+        final = valorDeEntrada / 1024
+        return final
+    }
+    if (diferencia == 2)
+    {
+        final = valorDeEntrada / 1024 / 1024
+        return final
+    }
+    if (diferencia == 3)
+    {
+        final = valorDeEntrada / 1024 / 1024 / 1024
+        return final
+    }
+    if (diferencia == 4)
+    {
+        final = valorDeEntrada / 1024 / 1024 / 1024 / 1024
+        return final
+    }
+
+
+        
+
+    // if (diferencia > 0)
+    // {
+    // for( contador = 1; contador <= diferencia; contador ++)
+    // {
+    //     if (diferencia > 1)
+    //     {
+    //     final = entrada / 1024
+    //     }
+    // }
+    // return final
+    // }
+    // if (diferencia == 0)
+    // {
+    //     final = 
+    // }
+
 }
 
 function calculo ()
 {
-
-   diferencia()
-   datoDeEntrada = document.getElementById("DatoE").value
-   datoDeSalida = document.getElementById("DatoS").value
+   datoDeEntrada = parseInt(document.getElementById("DatoE").value)
+   datoDeSalida = parseInt(document.getElementById("DatoS").value)
    valorDeEntrada = document.getElementById("ValorEntrada").value
 
-
-
-
-   valorDeSalida = 1
+   //console.log(diferencia(datoDeEntrada, datoDeSalida))
+   
+   valorDeSalida = diferencia(datoDeEntrada, datoDeSalida)
    document.getElementById("ValorSalida").value = valorDeSalida  
 }
 
 function iniciar()
 {
-//   datoDeEntrada = document.getElementById("DatoE").value
-//   datoDeSalida = document.getElementById("DatoS").value
-//   valorDeEntrada = document.getElementById("ValorEntrada").value
-//   valorDeSalida = document.getElementById("ValorSalida").value
-
-
   boton = document.getElementById("boton")
   boton.addEventListener("click", calculo)
 }
